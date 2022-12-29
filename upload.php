adc6d4fcb14dd87bbb9bf461e002d262bb2d399e
Panda.xyz <98690785+Pandaxyz-xd@users.noreply.github.com>
2022-12-29 21:02:21 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -10,11 +10,14 @@ if (isset($_FILES['mi-archivo'])) {
   move_uploaded_file($ruta_provisional, $src);
   $nombre_sin_prefijo = pathinfo($nombre, PATHINFO_FILENAME);
   $html = '<!DOCTYPE html>
-    <meta charset="UTF-8">
-    <meta name="author" content="Pandaxyz">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <html>
   <head>
+   <meta charset="UTF-8">
+   <meta name="description" content="Fran_afp_ file uploader">
+   <meta name="keywords" content="Uploader">
+   <meta name="author" content="Fran_afp_">
+   <meta name="theme-color" content="#62007d">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> archivo: '.$nombre_sin_prefijo.'</title>
   </head>
   <style>
